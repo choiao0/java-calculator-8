1a77c1a4b19ade401e49184227cee3bb275bf37d
choiao0 <qawse6@daum.net>
2025-10-20 13:34:07 +0900

    feat(Delimiter): 구분자가 양수가 아닌지 검증

--- src/main/java/calculator/model/Delimiter.java
+++ src/main/java/calculator/model/Delimiter.java
@@ -6,7 +6,14 @@ public class Delimiter {
     private final String delimiter;
 
     public Delimiter(String delimiter) {
+        validateDelimiter(delimiter);
         this.delimiter = delimiter;
+    }
+
+    private void validateDelimiter(String delimiter) {
+        if (delimiter.matches("[1-9]")) {
+            throw new IllegalArgumentException("양수는 구분자로 사용할 수 없습니다.");
+        }
     }
 
     public String getDelimiter() {
